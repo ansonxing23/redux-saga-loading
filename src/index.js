dc4da2ad4bfb3b8b8c66379952efd479b752c54f
Anson <junwen.xing@newtranx.com>
2018-07-23 10:37:03 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const NAMESPACE = 'loading';
+const ISLOADING = 'REDUX_SAGA_IS_LOADING'
+const NOTLOADING = 'REDUX_SAGA_NOT_LOADING'
+
+function createReduxSagaLoading(opts = {}) {
+    const namespace = opts.namespace || NAMESPACE;
+    const initialState = {
+        models: {}
+    };
+    const reduxReducers = {
+        [namespace](state = false, action) {
+            switch (action.type) {
+                case ISLOADING:
+                    return true;
+                case NOTLOADING:
+                    return false;
+                default:
+                    return state;
+            }
+        }
+    }
+    
+    return {
+        reduxReducers
+    };
+}
+
+const startLoading = () => yield put({ type: ISLOADING })
+const stopLoading = () => yield put({ type: NOTLOADING })
+
+export {
+    createReduxSagaLoading,
+    startLoading,
+    stopLoading
+}
